846b59c76df0543a8f2498d00ccc7e05213ce8ad
nudredx <luis.f.nunes6@aluno.senai.br>
2025-12-03 09:17:14 -0300

    Add files via upload

---
+++ src/test/java/com/senai/infoa/trabalho_indv/TrabalhoIndvApplicationTests.java
@@ -0,0 +1,13 @@
+package com.senai.infoa.trabalho_indv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrabalhoIndvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
