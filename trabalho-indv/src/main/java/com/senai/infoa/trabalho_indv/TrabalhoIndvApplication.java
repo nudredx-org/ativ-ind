4f6a4ad269894e5e5110c07066a38ca17920667f
nudredx <luis.f.nunes6@aluno.senai.br>
2025-12-03 09:14:55 -0300

    Add files via upload

---
+++ trabalho-indv/src/main/java/com/senai/infoa/trabalho_indv/TrabalhoIndvApplication.java
@@ -0,0 +1,13 @@
+package com.senai.infoa.trabalho_indv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrabalhoIndvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrabalhoIndvApplication.class, args);
+	}
+
+}
